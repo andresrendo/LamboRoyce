2a75b3877efa0bfc80f2a43cffdad54457fd684b
Andres Rendo <andres.rendo@correo.unimet.edu.ve>
2023-05-29 17:47:27 -0400

    actualizacion del txt

--- src/classes/Configuracion.java
+++ src/classes/Configuracion.java
@@ -4,36 +4,32 @@
  */
 package classes;
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Configuracion {
 
-    private Map<String, String> parametros;
+    private Map<String, Integer> parametros;
 
     public Configuracion() {
         parametros = new HashMap<>();
     }
 
     public void leerConfiguracion(String archivo) {
-        try {
-            File file = new File(archivo);
-            System.out.println("Ruta del archivo: " + file.getAbsolutePath());
-
-            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-                String linea;
-                while ((linea = br.readLine()) != null) {
-                    linea = linea.trim();
-                    if (!linea.isEmpty()) {
-                        String[] partes = linea.split("=");
-                        if (partes.length == 2) {
-                            String clave = partes[0].trim();
-                            String valor = partes[1].trim();
-                            parametros.put(clave, valor);
-                        }
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                linea = linea.trim();
+                if (!linea.isEmpty()) {
+                    String[] partes = linea.split("=");
+                    if (partes.length == 2) {
+                        String clave = partes[0].trim();
+                        int valor = Integer.parseInt(partes[1].trim());
+                        parametros.put(clave, valor);
                     }
                 }
             }
@@ -41,15 +37,45 @@ public class Configuracion {
             e.printStackTrace();
         }
     }
-    public String obtenerParametro(String clave) {
+
+    public void actualizarConfiguracion(String archivo) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(archivo))) {
+            for (Map.Entry<String, Integer> entry : parametros.entrySet()) {
+                String clave = entry.getKey();
+                int valor = entry.getValue();
+                bw.write(clave + "=" + valor);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int obtenerParametro(String clave) {
         return parametros.get(clave);
+    }
+
+    public void setParametro(String clave, int valor) {
+        parametros.put(clave, valor);
+    }
+
+    public void sumarParametro(String clave, int cantidad) {
+        int valorActual = obtenerParametro(clave);
+        int nuevoValor = valorActual + cantidad;
+        setParametro(clave, nuevoValor);
+    }
+
+    public void restarParametro(String clave, int cantidad) {
+        int valorActual = obtenerParametro(clave);
+        int nuevoValor = valorActual - cantidad;
+        setParametro(clave, nuevoValor);
     }
 
     public void imprimirConfiguracion() {
         System.out.println("Configuración:");
-        for (Map.Entry<String, String> entry : parametros.entrySet()) {
+        for (Map.Entry<String, Integer> entry : parametros.entrySet()) {
             String clave = entry.getKey();
-            String valor = entry.getValue();
+            int valor = entry.getValue();
             System.out.println(clave + ": " + valor);
         }
     }
